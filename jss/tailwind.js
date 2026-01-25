d4bd31f057905151d1868a9cdf69c9fab448a02b
22Q@3t28fBCgbvY <jesus.alberto.r.b.mx@gmail.com>
2026-01-25 16:16:07 -0600

    Segunda versión de Nancy Bodas

---
+++ jss/tailwind.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0DA797",
+        "background-light": "#F9DEB2",
+        "background-dark": "#0C354D",
+        secondary: "#7FBAB4",
+        accent: "#9E7F5E",
+      },
+      fontFamily: {
+        display: ["Cormorant Garamond", "serif"],
+        sans: ["Montserrat", "sans-serif"],
+        script: ["Great Vibes", "cursive"],
+      },
+      borderRadius: {
+        DEFAULT: "0.75rem",
+      },
+    },
+  },
+};
